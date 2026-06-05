4ca6c182b42fb1d5f5b44050caff2fab5483a84c
alexdc04 <alex1.dulles@gmail.com>
2026-06-04 21:01:39 -0400

    update route for task submission

--- server/db.js
+++ server/db.js
@@ -9,3 +9,16 @@ const dataPASS = process.env.SUPABASE_PASSWORD;
 
 // create client
 const supabaseClient = createClient(dataURL, dataPASS);
+
+// database functions
+
+// submit new task
+export async function taskSubmit(name, due) {
+    const {data, error} = await supabaseClient
+        .from('tasks')
+        .insert({ task_name: name, start_time: Date.now().toString(), due_date: due})
+        .select();
+    
+    if (error) throw new Error(error.message);
+    return data[0];
+}
--- server/server.js
+++ server/server.js
@@ -2,22 +2,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const db = require('./db.js');
+const cors = require('cors');
 
 const env = require('dotenv').config();
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 const data_URL = process.env.SUPABASE_LINK;
 const data_PASS = process.env.SUPABASE_PASSWORD;
+
+app.use(cors())
 
 // basic route on the home page
 app.get('/', (req, res) => {
   res.send('home.');
 });
 
-// test for a new page
-app.get('/test', (req, res) => {
-    res.send('test page.');
+// submit a new task
+app.post('/api/tasksubmit', (req, res) => {
+  const { task_name, start_time, due_date } = req.body;
+
+  // validate fields are full
+  if (!task_name || !start_time || !due_date) {
+    return res.status(400).json({ error: 'Each task needs a name, start time, and due date.'});
+  }
+
 });
 
 // begin server
